07b916d14864f7130b67c286ce2335f1394a63db
Vitor Simoes <vitor6427126@gmail.com>
2021-05-30 22:01:32 -0300

    First Commit

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -5,6 +5,16 @@ public class Ship : MonoBehaviour
 {
     [SerializeField] private Transform cannon;
     [SerializeField] private GameObject laser;
+    [SerializeField] private GameObject laser2;
+    [SerializeField] private GameObject laser3;
+    [SerializeField] private GameObject laser4;
+    [SerializeField] private GameObject laser5;
+    [SerializeField] private GameObject laser6;
+    [SerializeField] private GameObject laser7;
+    [SerializeField] private GameObject laser8;
+    [SerializeField] private GameObject laser9;
+    [SerializeField] private GameObject laser10;
+
 
     public float shootingTime = 0.1f;
     private bool shooting;
@@ -43,10 +53,21 @@ public class Ship : MonoBehaviour
 
         transform.position = new Vector3(clampX, clampY, transform.position.z);
 
-        if (Input.GetKey(KeyCode.Space)) Shot();
+
+        if(Input.GetKey(KeyCode.F1))Shot(laser);
+        if(Input.GetKey(KeyCode.F2))Shot(laser2);
+        if(Input.GetKey(KeyCode.F3))Shot(laser3);
+        if(Input.GetKey(KeyCode.F4))Shot(laser4);
+        if(Input.GetKey(KeyCode.F5))Shot(laser5);
+        if(Input.GetKey(KeyCode.F6))Shot(laser6);
+        if(Input.GetKey(KeyCode.F7))Shot(laser7);
+        if(Input.GetKey(KeyCode.F8))Shot(laser8);
+        if(Input.GetKey(KeyCode.F9))Shot(laser9);
+        if(Input.GetKey(KeyCode.F11))Shot(laser10);
+        
     }
 
-    void Shot ()
+    void Shot (GameObject laser)
     {
         if (shooting) return;
         shooting = true;
